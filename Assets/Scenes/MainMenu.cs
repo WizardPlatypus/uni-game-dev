27b048b911991a14522bc3e34c6674fa2daf7a48
WizardPlatypus <f.volik.m@gmail.com>
2025-05-27 23:49:24 +0300

    Added all files
    Signed-off-by: WizardPlatypus <f.volik.m@gmail.com>

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject authorPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowAuthorInfo()
+    {
+        authorPanel.SetActive(true);
+    }
+
+    public void CloseAuthorInfo()
+    {
+        authorPanel.SetActive(false);
+    }
+
+    public void LoadTrainingScene()
+    {
+        SceneManager.LoadScene("Training");
+    }
+}
